4544b50cde0107bbb045a5f71a5b3ffb1800e373
yoshifumi <yoshifumi1975@gmail.com>
2012-03-30 13:28:47 +0900

    add check_ml110.py

---
+++ python/check_ml110.py
@@ -0,0 +1,35 @@
+#!/bin/env python
+
+import httplib
+import re
+import smtplib
+
+URL='http://nttxstore.jp/_II_HP13894432'
+FROM='ykinoshi@redhat.com'
+TO='ykinoshi@redhat.com'
+SUBJECT='ML110 G7'
+
+
+def main():
+	price = get_price()
+	msg = 'From: %s\r\nTo:%s\r\nSubject:%s\r\n\r\n' % (FROM,TO,SUBJECT) + 'ML110 G7: ' + price + ' yen'
+	send_mail( FROM, TO, msg )
+
+def get_price():
+	conn = httplib.HTTPConnection("nttxstore.jp")
+	conn.request("GET", "/_II_HP13894432")
+	result = conn.getresponse()
+	cont = result.read()
+	price = re.search(r'.*<dd class="price"><span>(.*?00)', cont).group(1)
+	coupon = ' (+coupon)' if re.search(r'coupon', cont) else ''
+	return price + coupon
+
+def send_mail( fromaddr, toaddr, msg ):
+	server = smtplib.SMTP('int-mx.corp.redhat.com.')
+	server.sendmail(fromaddr, toaddr, msg)
+	server.quit()
+
+
+if __name__ == "__main__":
+	main()
+
