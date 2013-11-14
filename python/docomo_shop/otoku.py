ac885ac37b845e06fc04889f75d0f96efa4d5796
Yoshifumi Kinoshita <ykinoshi@redhat.com>
2013-11-14 17:21:18 +0900

    Add otoku.py

---
+++ python/docomo_shop/otoku.py
@@ -0,0 +1,36 @@
+#!/bin/env python
+
+import sys
+import httplib
+import re
+import smtplib
+
+URL='http://nttxstore.jp/_II_HP13894432'
+FROM= sys.argv[1] if len(sys.argv)>1 else 'ykinoshi@foo.example.com'
+TO= sys.argv[1] if len(sys.argv)>1 else 'ykinoshi@bar.example.com'
+SUBJECT='ML110 G7'
+
+
+def main():
+	price = get_price()
+	msg = 'From: %s\r\nTo:%s\r\nSubject:%s\r\n\r\n' % (FROM,TO,SUBJECT) + 'ML110 G7: ' + price + ' yen'
+	send_mail( FROM, TO, msg )
+
+def get_price():
+	conn = httplib.HTTPConnection("nttxstore.jp")
+	conn.request("GET", "/_II_HP13894432")
+	result = conn.getresponse()
+	cont = result.read()
+	price = re.search(r'.*<dd class="price"><span>(.*?00)', cont).group(1)
+	coupon = ' (+coupon)' if re.search(r'coupon', cont) else ''
+	return price + coupon
+
+def send_mail( fromaddr, toaddr, msg ):
+	server = smtplib.SMTP('smtp.foo.bar.example.com.')
+	server.sendmail(fromaddr, toaddr, msg)
+	server.quit()
+
+
+if __name__ == "__main__":
+	main()
+
