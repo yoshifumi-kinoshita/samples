2d49cba0bd7d0a5893a434b476fecbe01c902542
Yoshifumi Kinoshita <yoshifumi1975@gmail.com>
2021-08-13 17:17:48 -0700

    Remove unused headers

--- c/my_thread/my_thread.c
+++ c/my_thread/my_thread.c
@@ -1,30 +1,7 @@
 #include <stdlib.h>
 #include <stdio.h>
-#include <string.h>
-#include <strings.h>
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <sys/socket.h>
-#include <sys/wait.h>
-#include <syslog.h>
-#include <sys/ioctl.h>
-#include <sys/param.h>
-#include <arpa/inet.h>
 #include <unistd.h>
-#include <getopt.h>
-#include <dirent.h>
 #include <pthread.h>
-#include <errno.h>
-#include <netinet/in.h>
-#include <net/if.h>
-#include <net/if_arp.h>
-#include <net/route.h>
-#include <netinet/in.h>
-#include <netinet/if_ether.h>
-#include <netinet/ether.h>
-#include <linux/if_packet.h>
-#include <linux/if_ether.h>
-
 
 static int num_of_thread = 100;
 
